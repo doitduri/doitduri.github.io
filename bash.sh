84bd3524c2b1664dccd91cd40c954b91c305cca4
duri <kdle_@naver.com>
2020-02-22 16:21:23 +0900

    aaa

---
+++ bash.sh
@@ -0,0 +1,14 @@
+# build.sh
+pip install pipenv
+
+echo "Starting build"
+cd notion
+pipenv install
+
+echo "Get posts"
+pipenv run python get_blog_posts.py
+cd ..
+
+echo "Build frontend site"
+npm install
+npm run build
